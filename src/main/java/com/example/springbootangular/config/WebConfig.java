48809ce35ef7984fee1d204188f7bc8db9131ee7
raymondhalim <raymondhalimdotcom@gmail.com>
2020-02-13 19:28:37 +0800

    Add spring boot and angular modules.

---
+++ src/main/java/com/example/springbootangular/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.example.springbootangular.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/")
+                .setCachePeriod(0)
+                .addResourceLocations("classpath:/static/index.html")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) {
+                        if (!resourcePath.equals("/")) {
+                            return null;
+                        }
+                        return location.exists() && location.isReadable() ? location : null;
+                    }
+                });
+    }
+}
---
+++ ui/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Spring Boot and Angular Seed Project';
+  built = environment.envi;
+}
